874400ce18d6db1fb90a99c432c35f5a69ef40c2
Sandile Siwundla <127257029+sandileSiwundla@users.noreply.github.com>
2024-08-01 12:46:17 +0000

    Added permission requirement to corporate nft minting.

--- ERC721A.sol
+++ ERC721A.sol
@@ -8,6 +8,7 @@ contract Azuki is ERC721A {
     address public owner;
     address[] addressOfReserved;
     uint16 public countReserved = 0;
+    address[] corporateAddresses;
 
     constructor() ERC721A("Spacebear", "SBR") {
         owner = msg.sender;
@@ -15,18 +16,22 @@ contract Azuki is ERC721A {
 
     function mint(address to) external payable {
         // `_mint`'s second argument now takes in a `quantity`, not a `tokenId`.
-        uint256 MaxSupply = 85;
+        uint256 MaxSupply = 90;
         
 
-        if (countReserved<10 && isAddressInList(to)) {
+        if (countReserved<10 && isReserved(to)) {
+
+            require(msg.value == 1 ether, "Insufficient funds. funds must be greater than 1");
 
             require(!addressOwnsNFT(to), "one token one address");
             
             _mint(to, 1);
-        }else{
-            require(msg.value > 1 ether, "Insufficient funds. funds must be greater than 1");
 
-            require(totalSupply() < MaxSupply, "Value must be greater than zero");
+            countReserved++;
+        }else{
+            require(msg.value == 1 ether, "Insufficient funds. funds must be greater than 1");
+
+            require(totalSupply() < MaxSupply, "Exceeded amount of NFT's mintable.");
 
             require(!addressOwnsNFT(to), "one token one address");
 
@@ -34,9 +39,20 @@ contract Azuki is ERC721A {
         }
     
     }
-    function isAddressInList(address _address) public view returns (bool) {
+
+
+    function isReserved(address _address) public view returns (bool) {
         for (uint256 i = 0; i < addressOfReserved.length; i++) {
             if (addressOfReserved[i] == _address) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function isCorporateAddress(address user) public view returns (bool) {
+        for (uint256 i = 0; i < corporateAddresses.length; i++) {
+            if (corporateAddresses[i] == user) {
                 return true;
             }
         }
@@ -47,15 +63,19 @@ contract Azuki is ERC721A {
         // `_mint`'s second argument now takes in a `quantity`, not a `tokenId`.
         uint256 MaxSupply = 5;
 
-        require(msg.value > 5 ether, "Insufficient funds. funds must be greater than 5");
+        require(isCorporateAddress(to), "Only corporate can mint corporate NFTs.");
+
+        require(msg.value == 5 ether, "Insufficient funds. funds must be greater than 5");
         
         require(totalSupply()< MaxSupply, "Value must be greater than zero");
 
         require(!addressOwnsNFT(to), "one token one address");
 
-        require(owner == to , "Only one token per address");
         _mint(owner, 1);
+        
     }
+
+
 
      function addressOwnsNFT(address user) public view returns (bool) {
         uint256 balance = balanceOf(user);
@@ -63,12 +83,18 @@ contract Azuki is ERC721A {
     }
 
     function _baseURI() internal pure override returns (string memory) {
-        return 'https://ethereum-blockchain-developer.com/2022-06-nft-truffle-hardhat-foundry/nftdata/';
+        return 'https://white-wrong-kangaroo-454.mypinata.cloud/ipfs/QmWabUE8z3p4U7yZ7y2LpPgLHhCrCdWvsxe3C5ssUGyPtX/';
     }
 
     function addReservedAddress(address user) public {
-        require(address(this) == owner, "You do not have rights to add address");
+        require(msg.sender == owner, "You do not have rights to add address");
         addressOfReserved.push(user);
     }
+
+    function addCorporateAddress(address to) external  {
+        require(msg.sender == owner, "Only address owner can grant access.");
+        corporateAddresses.push(to);
+    }
+
 
 }
